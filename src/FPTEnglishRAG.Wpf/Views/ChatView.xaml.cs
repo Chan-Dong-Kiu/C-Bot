9e4be56b94ff682e3b62c3ee31db5e386b39cb84
Hungle2910 <lhung291005@gmail.com>
2026-08-24 23:13:12 +0700

    fix(chat-ui): send question on Enter and insert newline on Shift+Enter

--- src/FPTEnglishRAG.Wpf/Views/ChatView.xaml.cs
+++ src/FPTEnglishRAG.Wpf/Views/ChatView.xaml.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Windows.Controls;
+using System.Windows.Input;
+using FPTEnglishRAG.Wpf.ViewModels;
 
 namespace FPTEnglishRAG.Wpf.Views;
 
@@ -7,5 +10,30 @@ public partial class ChatView : UserControl
     public ChatView()
     {
         InitializeComponent();
+    }
+
+    private void QuestionTextBox_PreviewKeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter || e.Key == Key.Return)
+        {
+            if (Keyboard.Modifiers.HasFlag(ModifierKeys.Shift))
+            {
+                // Shift + Enter -> insert newline
+                var textBox = (TextBox)sender;
+                int caretIndex = textBox.SelectionStart + Environment.NewLine.Length;
+                textBox.SelectedText = Environment.NewLine;
+                textBox.CaretIndex = caretIndex;
+                e.Handled = true;
+            }
+            else
+            {
+                // Enter -> send question
+                e.Handled = true;
+                if (DataContext is ChatViewModel viewModel && viewModel.SendCommand.CanExecute(null))
+                {
+                    viewModel.SendCommand.Execute(null);
+                }
+            }
+        }
     }
 }
